1235c079ec18ad6e261c2e9a473e708074956c95
gdkid <77850955+fuh-Q@users.noreply.github.com>
2025-08-13 12:16:05 -0400

    supply common imports

--- animations.js
+++ animations.js
@@ -3,3 +3,16 @@
  * to run an animation, you `export default` a function in the form `() => Promise<Set<Animation>>`
  * where the Animation is any of the exported classes from `./drawing.js
  */
+
+import {
+    InflateDeflate,
+    LineAnimation,
+    DottedLineAnimation,
+    Blink,
+    MapViewAdjustment,
+    Rotation,
+    Pop,
+    SetSourceCoords,
+    LinearAdjustNumericPaintProp,
+    Script,
+} from "./drawing.js";
